e52927c2a5433a970ce60dccb850712fc6294163
PatelKamya <kamyapatel303@gmail.com>
2026-01-20 13:57:45 +0530

    new small updates

--- test/src/components/header.jsx
+++ test/src/components/header.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import { TiSpiral } from "react-icons/ti";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import { FiMail } from "react-icons/fi";
+import { IoNotificationsOutline } from "react-icons/io5";
+
+
 
 const Header = () => {
-  return (
-    <div>
-        <Navbar className=''>
-            <Container>
-                <Navbar.Brand>Donezo</Navbar.Brand>
-            </Container>
-        </Navbar>
-    </div>
-  )
+    return (
+        <div className=''>
+            <div className='rounded-3 m-4' style={{ backgroundColor: 'gray' }}>
+                <Navbar className='d-flex justify-content-between me-2'>
+                    <div className='d-flex gap-1 m-1'>
+                        <div><TiSpiral className='' size={40} /></div>
+                        <Navbar.Brand className=''>Donezo</Navbar.Brand>
+                    </div>
+                    <div className='d-flex gap-2'>
+                        <div><FiMail /></div>
+                        <div><IoNotificationsOutline /></div>
+                    </div>
+                    <div className='profile-container'>
+
+                    </div>
+                </Navbar>
+            </div>
+        </div>
+    )
 }
 
 export default Header
